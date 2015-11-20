1a67e310c4b5503d55fa65ed48ac388e621bbfcc
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-20 16:47:27 +0000

    use heroku url for test

--- tests/test_it.py
+++ tests/test_it.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_notifications_admin_index():
-    response = requests.request("GET", "http://localhost:6012/index")
+    response = requests.request("GET", "http://notifications-admin.herokuapp.com/index")
     assert response.status_code == 200
     assert response.content == 'Hello from notifications-admin'
