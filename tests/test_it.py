6a55e46afa6068041548979b243f4ad346bb3864
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-20 16:32:47 +0000

    Test notifications-admin index page

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,2 +1,7 @@
-def test_it():
-    assert 1 == 1
+import requests
+
+
+def test_notifications_admin_index():
+    response = requests.request("GET", "http://localhost:6012/index")
+    assert response.status_code == 200
+    assert response.content == 'Hello from notifications-admin'
