41221b36a596b1253445f1e49b10bff1fc44be42
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-26 10:42:08 +0000

    Fix test for initial registration page flow

--- tests/test_it.py
+++ tests/test_it.py
@@ -2,6 +2,7 @@ import requests
 
 
 def test_notifications_admin_index():
-    response = requests.request("GET", "http://notifications-admin.herokuapp.com/index")
+    # response = requests.request("GET", "http://localhost:6012")
+    response = requests.request("GET", "http://notifications-admin.herokuapp.com/")
     assert response.status_code == 200
-    # assert response.content == 'Hello from notifications-admin'
+    assert 'GOV.UK Notify' in response.content
