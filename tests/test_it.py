bf9addce584961e30456c74b767afe05ca5dbb71
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-26 11:23:16 +0000

    Convert bytes to str for assertion

--- tests/test_it.py
+++ tests/test_it.py
@@ -5,4 +5,4 @@ def test_notifications_admin_index():
     # response = requests.request("GET", "http://localhost:6012")
     response = requests.request("GET", "http://notifications-admin.herokuapp.com/")
     assert response.status_code == 200
-    assert 'GOV.UK Notify' in response.content
+    assert 'GOV.UK Notify' in str(response.content)
