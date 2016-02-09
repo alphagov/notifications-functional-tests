a042954be86781d29debf65a780cec0e3967f1cf
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-02-09 15:03:35 +0000

    The test works on preview

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ from tests.utils import get_sms, delete_sms_messge
 
 
 def test_utils():
+    # # Convience method to get and delete sms messages
     # messages = get_sms()
     # print(len(messages))
     # for m in messages:
