bad55da14cb24924ad267c5be7b9994d3ad0b308
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-02-09 10:54:47 +0000

    Correction to Travis environment variables

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from tests.utils import get_sms, delete_sms_messge
+
+
+def test_utils():
+    # messages = get_sms()
+    # print(len(messages))
+    # for m in messages:
+    #     delete_sms_messge(m.sid)
+    pass
