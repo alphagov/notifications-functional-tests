b2f758edd8453529dcbb9354903a1cee01dab7a7
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-02-09 11:19:02 +0000

    fix codestyle

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,9 @@ from tests.utils import get_sms, delete_sms_messge
 
 
 def test_utils():
-    messages = get_sms()
-    print(len(messages))
-    for m in messages:
-        print(m.body)
-        # delete_sms_messge(m.sid)
-
+    # messages = get_sms()
+    # print(len(messages))
+    # for m in messages:
+    #     print(m.body)
+    #     delete_sms_messge(m.sid)
+    pass
