6c10b19cf8a670a17ccaa07d5b1be2c049bd4def
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-03-31 16:44:17 +0100

    Python client tests added and working on preview. Not working on live until code is aligned.

--- config.py
+++ config.py
@@ -10,10 +10,11 @@ class Config(object):
     TWILIO_AUTH_TOKEN = os.environ[os.environ['ENVIRONMENT'] + '_TWILIO_AUTH_TOKEN']
     FUNCTIONAL_TEST_EMAIL = os.environ[os.environ['ENVIRONMENT'] + '_FUNCTIONAL_TEST_EMAIL']
     FUNCTIONAL_TEST_PASSWORD = os.environ[os.environ['ENVIRONMENT'] + '_FUNCTIONAL_TEST_PASSWORD']
-    FUNCTIONAL_TEMPLATE_ID = os.environ[os.environ['ENVIRONMENT'] + '_TEMPLATE_ID']
+    FUNCTIONAL_EMAIL_TEMPLATE_ID = os.environ[os.environ['ENVIRONMENT'] + '_EMAIL_TEMPLATE_ID']
+    FUNCTIONAL_SMS_TEMPLATE_ID = os.environ[os.environ['ENVIRONMENT'] + '_SMS_TEMPLATE_ID']
     FUNCTIONAL_SERVICE_ID = os.environ[os.environ['ENVIRONMENT'] + '_SERVICE_ID']
     FUNCTIONAL_API_KEY = os.environ[os.environ['ENVIRONMENT'] + '_API_KEY']
-    EMAIL_FOLDER = 'notify'
-    REGISTRATION_EMAIL = 'registration'
+    EMAIL_NOTIFICATION_LABEL = 'notify'
+    REGISTRATION_EMAIL_LABEL = 'registration'
     EMAIL_TRIES = 10
     EMAIL_DELAY = 5
--- tests/test_python_client_flow.py
+++ tests/test_python_client_flow.py
@@ -1,21 +1,51 @@
 from requests import session
 
 from config import Config
-from tests.utils import get_sms_via_heroku
+from tests.utils import (
+    get_sms_via_heroku,
+    get_email_body,
+    remove_all_emails)
 from notifications_python_client.notifications import NotificationsAPIClient
 
 
-def test_python_client():
+def _create_client():
     client = NotificationsAPIClient(
         Config.NOTIFY_API_URL,
         Config.FUNCTIONAL_SERVICE_ID,
         Config.FUNCTIONAL_API_KEY)
+    return client
+
+
+def test_python_client_sms():
+    client = _create_client()
     resp_json = client.send_sms_notification(
         Config.TWILIO_TEST_NUMBER,
-        Config.FUNCTIONAL_TEMPLATE_ID)
+        Config.FUNCTIONAL_SMS_TEMPLATE_ID)
+    print(resp_json)
     assert 'result' not in resp_json['data']
     notification_id = resp_json['data']['notification']['id']
     message = get_sms_via_heroku(session())
     assert "The quick brown fox jumped over the lazy dog" in message
     resp_json = client.get_notification_by_id(notification_id)
-    assert resp_json['data']['notification']['status'] == 'sent'
+    assert resp_json['data']['notification']['status'] in ['sent', 'delivered']
+
+
+def test_python_client_email():
+    remove_all_emails(email_folder=Config.EMAIL_NOTIFICATION_LABEL)
+    client = _create_client()
+    try:
+        resp_json = client.send_email_notification(
+            Config.FUNCTIONAL_TEST_EMAIL,
+            Config.FUNCTIONAL_EMAIL_TEMPLATE_ID)
+        print(resp_json)
+        assert 'result' not in resp_json['data']
+        notification_id = resp_json['data']['notification']['id']
+        message = get_email_body(
+            Config.FUNCTIONAL_TEST_EMAIL,
+            Config.FUNCTIONAL_TEST_PASSWORD,
+            Config.EMAIL_NOTIFICATION_LABEL)
+    finally:
+        remove_all_emails(email_folder=Config.EMAIL_NOTIFICATION_LABEL)
+    assert "The quick brown fox jumped over the lazy dog" in message
+    resp_json = client.get_notification_by_id(notification_id)
+    assert resp_json['data']['notification']['status'] in ['sent', 'delivered']
