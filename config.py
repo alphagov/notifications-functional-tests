2192d815077759b92231c806429d50a41890153a
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-04-22 14:48:55 +0100

    Remove config that are no longer used.

--- config.py
+++ config.py
@@ -11,10 +11,6 @@ class Config(object):
     FUNCTIONAL_TEST_NAME = os.environ['ENVIRONMENT'] + '_Functional Test_'
     FUNCTIONAL_TEST_EMAIL = os.environ[os.environ['ENVIRONMENT']+'_FUNCTIONAL_TEST_EMAIL']
     FUNCTIONAL_TEST_PASSWORD = os.environ[os.environ['ENVIRONMENT'] + '_FUNCTIONAL_TEST_PASSWORD']
-    FUNCTIONAL_EMAIL_TEMPLATE_ID = os.environ[os.environ['ENVIRONMENT'] + '_EMAIL_TEMPLATE_ID']
-    FUNCTIONAL_SMS_TEMPLATE_ID = os.environ[os.environ['ENVIRONMENT'] + '_SMS_TEMPLATE_ID']
-    FUNCTIONAL_SERVICE_ID = os.environ[os.environ['ENVIRONMENT'] + '_SERVICE_ID']
-    FUNCTIONAL_API_KEY = os.environ[os.environ['ENVIRONMENT'] + '_API_KEY']
     EMAIL_NOTIFICATION_LABEL = 'notify'
     REGISTRATION_EMAIL_LABEL = 'registration'
     INVITATION_EMAIL_LABEL = 'invite'
