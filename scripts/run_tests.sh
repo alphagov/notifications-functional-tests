46b03f03cc38d556d246cfc136103b4fb5aaf92b
Adam Shimali <adam.shimali@gmail.com>
2016-05-09 14:09:10 +0100

    Try this to allow dependent build by env var

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -26,24 +26,23 @@ function display_result {
 pep8 .
 display_result $? 1 "Code style check"
 
-if [ "$#" -eq 1 ]; then
-  export ENVIRONMENT=$1
-  case $1 in
-      staging)
-        echo 'Running staging tests'
-        py.test -v -x tests/stating_live/test_send_notifications_from_csv.py
-        ;;
-      live)
-        echo 'Nothing to run on live yet'
-        ;;
-      preview|*)
-        echo 'Default test run - for preview and' $ENVIRONMENT
-        # Note registration *must* run before any other tests as it registers the user for use
-        # in later tests and test_python_client_flow.py needs to run last as it will use templates created
-        # by sms and email tests
-        py.test -v -x tests/test_registration.py tests/test_send_sms_from_csv.py tests/test_send_email_from_csv.py tests/test_invite_new_user.py tests/test_python_client_flow.py
-        ;;
-  esac
-fi
+: "${ENVIRONMENT:?Need to set ENVIRONMENT variable to non empty value}"
+
+case $ENVIRONMENT in
+    staging)
+      echo 'Running staging tests'
+      py.test -v -x tests/stating_live/test_send_notifications_from_csv.py
+      ;;
+    live)
+      echo 'Nothing to run on live yet'
+      ;;
+    preview|*)
+      echo 'Default test run - for preview and' $ENVIRONMENT
+      # Note registration *must* run before any other tests as it registers the user for use
+      # in later tests and test_python_client_flow.py needs to run last as it will use templates created
+      # by sms and email tests
+      py.test -v -x tests/test_registration.py tests/test_send_sms_from_csv.py tests/test_send_email_from_csv.py tests/test_invite_new_user.py tests/test_python_client_flow.py
+      ;;
+esac
 
 display_result $? 3 "Unit tests"
