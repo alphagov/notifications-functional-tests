a6a7727177526a4fb7f305ecd3cd6f4d6a4dac48
Adam Shimali <adam.shimali@gmail.com>
2016-05-11 17:01:14 +0100

    Add separate script for running smoke test

--- smoke_tester.py
+++ smoke_tester.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 
-print("Give me something to do!")
+import os
+import json
+import requests
+
+
+data = {"request":
+        {"branch": "master",
+         "config":
+            {"env":
+                {"global": ["TEST=smoketest"]},
+                "script": "./scripts/run_smoke_tests.sh"
+             }
+         }
+        }
+
+
+headers = {'Content-Type': 'application/json',
+           'Accept': 'application/json',
+           'Travis-API-Version': '3',
+           'Authorization': 'token ' + os.environ['TRAVIS_AUTH_TOKEN']
+           }
+url = "https://api.travis-ci.org/repo/alphagov/notifications-functional-tests/requests"
+resp = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(resp.status_code)
+print(resp.json())
