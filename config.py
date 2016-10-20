2983a1440ad0532b957f089b381229604e7cae5e
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2016-10-20 10:57:13 +0100

    make all env vars other than 'ENVIRONMENT' optional

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 
 class Config(object):
     ENVIRONMENT = os.environ['ENVIRONMENT']
-    NOTIFY_ADMIN_URL = os.environ[os.environ['ENVIRONMENT'] + '_NOTIFY_ADMIN_URL']
-    NOTIFY_API_URL = os.environ[os.environ['ENVIRONMENT'] + '_NOTIFY_API_URL']
-    TEST_NUMBER = os.environ[os.environ['ENVIRONMENT'] + '_TEST_NUMBER']
-    FUNCTIONAL_TEST_NAME = os.environ['ENVIRONMENT'] + '_Functional Test_'
-    FUNCTIONAL_TEST_EMAIL = os.environ[os.environ['ENVIRONMENT']+'_FUNCTIONAL_TEST_EMAIL']
-    FUNCTIONAL_TEST_PASSWORD = os.environ[os.environ['ENVIRONMENT'] + '_FUNCTIONAL_TEST_PASSWORD']
-    FUNCTIONAL_TEST_EMAIL_PASSWORD = os.environ[os.environ['ENVIRONMENT'] + '_FUNCTIONAL_TEST_EMAIL_PASSWORD']
+    NOTIFY_ADMIN_URL = os.environ.get(os.environ.get('ENVIRONMENT') + '_NOTIFY_ADMIN_URL')
+    NOTIFY_API_URL = os.environ.get(os.environ.get('ENVIRONMENT') + '_NOTIFY_API_URL')
+    TEST_NUMBER = os.environ.get(os.environ.get('ENVIRONMENT') + '_TEST_NUMBER')
+    FUNCTIONAL_TEST_NAME = os.environ.get('ENVIRONMENT') + '_Functional Test_'
+    FUNCTIONAL_TEST_EMAIL = os.environ.get(os.environ.get('ENVIRONMENT')+'_FUNCTIONAL_TEST_EMAIL')
+    FUNCTIONAL_TEST_PASSWORD = os.environ.get(os.environ.get('ENVIRONMENT') + '_FUNCTIONAL_TEST_PASSWORD')
+    FUNCTIONAL_TEST_EMAIL_PASSWORD = os.environ.get(os.environ.get('ENVIRONMENT') + '_FUNCTIONAL_TEST_EMAIL_PASSWORD')
     EMAIL_NOTIFICATION_LABEL = 'notify'
     REGISTRATION_EMAIL_LABEL = 'registration'
     INVITATION_EMAIL_LABEL = 'invite'
@@ -18,9 +18,9 @@ class Config(object):
     RETRY_DELAY = 5
     PROVIDER_RETRY_TIMES = 12
     PROVIDER_RETRY_INTERVAL = 20
-    FUNCTIONAL_TEST_SERVICE_NAME = os.environ['ENVIRONMENT'] + '_Functional Test Service_'
-    NOTIFY_SERVICE_ID = os.environ[os.environ['ENVIRONMENT'] + '_NOTIFY_SERVICE_ID']
-    NOTIFY_SERVICE_API_KEY = os.environ[os.environ['ENVIRONMENT'] + '_NOTIFY_SERVICE_API_KEY']
+    FUNCTIONAL_TEST_SERVICE_NAME = os.environ.get('ENVIRONMENT') + '_Functional Test Service_'
+    NOTIFY_SERVICE_ID = os.environ.get(os.environ.get('ENVIRONMENT') + '_NOTIFY_SERVICE_ID')
+    NOTIFY_SERVICE_API_KEY = os.environ.get(os.environ.get('ENVIRONMENT') + '_NOTIFY_SERVICE_API_KEY')
     REGISTRATION_TEMPLATE_ID = 'ece42649-22a8-4d06-b87f-d52d5d3f0a27'
     INVITATION_TEMPLATE_ID = '4f46df42-f795-4cc4-83bb-65ca312f49cc'
     VERIFY_CODE_TEMPLATE_ID = '36fb0730-6259-4da1-8a80-c8de22ad4246'
