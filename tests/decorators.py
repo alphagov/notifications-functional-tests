6c8cece99aaf0a51dace36f7908f56eb23f22ef8
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2016-12-09 17:22:48 +0000

    Retry 5 times on staleelement exception

--- tests/decorators.py
+++ tests/decorators.py
@@ -1,12 +1,17 @@
-from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException
 
 
 def retry_on_stale_element_exception(func):
     def wrapped(*args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-        except StaleElementReferenceException:
-            result = func(*args, **kwargs)
-        return result
+        retry = 5
+        while retry:
+            try:
+                result = func(*args, **kwargs)
+            except StaleElementReferenceException as e:
+                if retry:
+                    retry -= 1
+                    continue
+                raise e
+            return result
 
     return wrapped
