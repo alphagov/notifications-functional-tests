535271c5f5175873413ac8b264d4d444ce3532d8
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2019-05-21 18:04:20 +0100

    Test precompiled validation fail

--- tests/functional/preview_and_dev/test_notify_api_letter.py
+++ tests/functional/preview_and_dev/test_notify_api_letter.py
@@ -9,7 +9,7 @@ from tests.postman import (
     send_precompiled_letter_via_api,
 )
 
-from tests.functional.preview_and_dev.consts import multi_page_pdf, pdf_with_virus
+from tests.functional.preview_and_dev.consts import multi_page_pdf, one_page_pdf, pdf_with_virus
 from tests.test_utils import assert_notification_body, recordtime, NotificationStatuses
 
 
@@ -64,6 +64,30 @@ def test_send_precompiled_letter_with_virus_notification_via_api(seeded_client_u
         reference,
         seeded_client_using_test_key,
         BytesIO(base64.b64decode(pdf_with_virus))
+    )
+
+    notification = retry_call(
+        get_notification_by_id_via_api,
+        fargs=[seeded_client_using_test_key, notification_id, NotificationStatuses.PENDING_VIRUS_CHECK],
+        tries=config['notification_retry_times'],
+        delay=config['notification_retry_interval']
+    )
+
+    assert reference == notification['reference']
+
+
+@recordtime
+def test_send_precompiled_letter_validation_failed_notification_via_api(seeded_client_using_test_key):
+
+    # This uses a file which drops the Eicar test virus into the temp directory
+    # The dropper code _should_ be detected before the eicar virus
+
+    reference = config['service_name'].replace(" ", "_") + "_validation_failed"
+
+    notification_id = send_precompiled_letter_via_api(
+        reference,
+        seeded_client_using_test_key,
+        BytesIO(base64.b64decode(one_page_pdf))
     )
 
     notification = retry_call(
