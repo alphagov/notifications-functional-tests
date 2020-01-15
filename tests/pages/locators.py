80d192f89c02838e56ec5b1745c523ad3404e841
Tom Byers <tombaromba@gmail.com>
2020-01-15 11:31:51 +0000

    Update locator for upload CSV page too

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -52,7 +52,7 @@ class EditTemplatePageLocators(object):
 
 class UploadCsvLocators(object):
     FILE_INPUT = (By.ID, 'file')
-    SEND_BUTTON = (By.CSS_SELECTOR, '[type=submit]')
+    SEND_BUTTON = (By.CSS_SELECTOR, 'main [type=submit]')
     FIRST_NOTIFICATION_AFTER_UPLOAD = (By.CLASS_NAME, 'table-row')
 
 
