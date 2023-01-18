5e128bc3b8d7f9e66b7c092aaeb7cce851881996
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-01-18 17:25:52 +0000

    split test_registration_and_invite into multiple tests
    now there's one fixture that invites, and is invoked once for the
    module. then all the separate bits are in separate unit tests to make it
    clearer what's running

--- tests/functional/preview_and_dev/test_registration_and_invite.py
+++ tests/functional/preview_and_dev/test_registration_and_invite.py
@@ -10,39 +10,31 @@ from tests.test_utils import (
 )
 
 
+@pytest.fixture(scope="module", autouse=True)
+@recordtime
+def register_user(_driver):
+    # has to use _driver as this is at module level (`driver` fixture is at function level, and just handles taking
+    # the screenshot on failure)
+    do_user_registration(_driver)
+
+
 @recordtime
 @pytest.mark.xdist_group(name="registration-flow")
-def test_registration_and_invite_flow(driver):
-    do_user_registration(driver)
-    do_user_can_add_reply_to_email_to_service(driver)
-    do_user_can_update_reply_to_email_to_service(driver)
-    do_user_can_update_sms_sender_of_service(driver)
-    do_user_can_add_sms_sender_to_service(driver)
+def test_invite_flow(driver):
     do_user_can_invite_someone_to_notify(driver, basic_view=False)
     do_user_can_invite_someone_to_notify(driver, basic_view=True)
 
 
-def do_user_can_update_sms_sender_of_service(driver):
-    dashboard_page = DashboardPage(driver)
-    dashboard_page.go_to_dashboard_for_service()
-
-    service_id = dashboard_page.get_service_id()
-
-    sms_sender_page = SmsSenderPage(driver)
-
-    sms_sender_page.go_to_text_message_senders(service_id)
-    sms_sender_page.click_change_link_for_first_sms_sender()
-    sms_sender_page.insert_sms_sender("first")
-    sms_sender_page.click_save_sms_sender()
-
-    main_content = sms_sender_page.get_sms_senders()
-
-    assert "first \u2002 (default)" in main_content.text
-
-    dashboard_page.go_to_dashboard_for_service(service_id)
+@recordtime
+@pytest.mark.xdist_group(name="registration-flow")
+def test_can_add_and_update_reply_to(driver):
+    do_user_can_add_reply_to_email_to_service(driver)
+    do_user_can_update_reply_to_email_to_service(driver)
 
 
-def do_user_can_add_sms_sender_to_service(driver):
+@recordtime
+@pytest.mark.xdist_group(name="registration-flow")
+def test_user_can_add_sms_sender_to_service(driver):
     dashboard_page = DashboardPage(driver)
     dashboard_page.go_to_dashboard_for_service()
 
@@ -59,5 +51,27 @@ def do_user_can_add_sms_sender_to_service(driver):
     assert "first \u2002 (default)" in main_content.text
     assert "second" in main_content.text
     assert "second \u2002 (default)" not in main_content.text
+
+    dashboard_page.go_to_dashboard_for_service(service_id)
+
+
+@recordtime
+@pytest.mark.xdist_group(name="registration-flow")
+def test_can_update_sms_sender_of_service(driver):
+    dashboard_page = DashboardPage(driver)
+    dashboard_page.go_to_dashboard_for_service()
+
+    service_id = dashboard_page.get_service_id()
+
+    sms_sender_page = SmsSenderPage(driver)
+
+    sms_sender_page.go_to_text_message_senders(service_id)
+    sms_sender_page.click_change_link_for_first_sms_sender()
+    sms_sender_page.insert_sms_sender("first")
+    sms_sender_page.click_save_sms_sender()
+
+    main_content = sms_sender_page.get_sms_senders()
+
+    assert "first \u2002 (default)" in main_content.text
 
     dashboard_page.go_to_dashboard_for_service(service_id)
