063c8fb0b243b0e548050db0d2a84797588ea661
Samuel Williams <samuelhwilliams@gmail.com>
2023-08-17 13:45:41 +0100

    Add functional test validating QR code data

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM joyzoursky/python-chromedriver:3.9-selenium
 
+RUN apt install -y poppler-utils libzbar0
+
 WORKDIR /var/project
 COPY . .
 RUN make bootstrap
--- tests/functional/preview_and_dev/test_notify_api_letter.py
+++ tests/functional/preview_and_dev/test_notify_api_letter.py
@@ -1,7 +1,10 @@
 import base64
+import uuid
 from io import BytesIO
 
+import pdf2image
 import pytest
+import pyzbar.pyzbar
 from retry.api import retry_call
 
 from config import config
@@ -11,6 +14,7 @@ from tests.functional.preview_and_dev.consts import (
 )
 from tests.postman import (
     get_notification_by_id_via_api,
+    get_pdf_for_letter_via_api,
     send_notification_via_api,
     send_precompiled_letter_via_api,
 )
@@ -42,6 +46,41 @@ def test_send_letter_notification_via_api(client_test_key):
         delay=config["notification_retry_interval"],
     )
     assert_notification_body(notification_id, notification)
+
+
+@recordtime
+@pytest.mark.xdist_group(name="api-letters")
+def test_send_letter_notification_with_qr_code_via_api(client_test_key):
+    qr_code_data = (
+        # 499 bytes of data
+        f"prefix {config['notify_admin_url']}/{'0123456789' * 38}/{uuid.uuid4()}/?code={uuid.uuid4()} suffix"
+    )
+
+    # Re-use the letter template, but we need a new paragraph for the QR code to be generated so put some newlines up
+    # front. Newlines at the end move the fullstop which is part of the template out of the QR code data.
+    notification = client_test_key.send_letter_notification(
+        config["service"]["templates"]["letter"],
+        {**config["letter_contact_data"], "build_id": f"\n\nqr: {qr_code_data}\n\n"},
+    )
+    notification_id = notification["id"]
+
+    retry_call(
+        get_notification_by_id_via_api,
+        fargs=[
+            client_test_key,
+            notification_id,
+            NotificationStatuses.RECEIVED,
+        ],
+        tries=config["notification_retry_times"],
+        delay=config["notification_retry_interval"],
+    )
+
+    letter_pdf = get_pdf_for_letter_via_api(client_test_key, notification_id)
+    image = pdf2image.convert_from_bytes(letter_pdf.read())
+    qr_codes = pyzbar.pyzbar.decode(image[0])
+
+    assert len(qr_codes) == 1, f"Expected to find 1 qr code in letter - found {len(qr_codes)}?"
+    assert qr_codes[0].data == qr_code_data.encode()
 
 
 @recordtime
