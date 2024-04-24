c3f8c0fbed8a09168e44c0762f78ef4bcba146a1
Chris Hill-Scott <me@quis.cc>
2024-04-24 15:57:19 +0100

    Update labels for editing a letter template
    To match https://github.com/alphagov/notifications-admin/pull/5021/files

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -67,8 +67,8 @@ class RenameTemplatePageLocators(object):
 class ViewLetterTemplatePageLocators(object):
     RENAME_LINK = (By.CLASS_NAME, "folder-heading-manage-link")
     EDIT_BODY = (By.CLASS_NAME, "edit-template-link-letter-body")
-    EDIT_WELSH_BODY = (By.XPATH, "//a/span[contains(text(), 'Welsh pages')]/..")
-    EDIT_ENGLISH_BODY = (By.XPATH, "//a/span[contains(text(), 'English pages')]/..")
+    EDIT_WELSH_BODY = (By.XPATH, "//a/span[contains(text(), 'Welsh body')]/..")
+    EDIT_ENGLISH_BODY = (By.XPATH, "//a/span[contains(text(), 'English body')]/..")
     ATTACH_BUTTON = (By.CLASS_NAME, "edit-template-link-attachment")
     SEND_BUTTON = (By.CLASS_NAME, "edit-template-link-get-ready-to-send")
     CHANGE_LANGUAGE = (By.LINK_TEXT, "Change language")
