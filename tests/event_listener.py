4c85ee68378c89781518314b5b83623de1cca4ae
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-04 18:36:08 +0000

    log all selenium activity and print out last N actions on error
    Selenium has an event listener base class[^1] - create a subclass of
    this that logs every time we perform basically any selenium event.
    
    Then, if there's an error, we print out the last 20 events the selenium
    web driver performed
    
    This will help make debugging easier - We already have some idea of
    "What page was the test viewing when it failed" and "what was it trying
    to do at time of failure", but if we realise it's on the wrong page,
    it's quite hard for us to tell how it got into that state - this commit
    should help with debugging that.
    
    [^1]: https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.support.abstract_event_listener

---
+++ tests/event_listener.py
@@ -0,0 +1,64 @@
+# ruff: noqa: T201
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import Any
+from urllib.parse import urlsplit, urlunsplit
+
+from selenium.webdriver.support.events import AbstractEventListener
+
+
+class EventType(StrEnum):
+    NAVIGATE_TO = "NAVIGATE_TO"
+    FIND = "FIND"
+    CLICK = "CLICK"
+    CHANGE_VALUE_OF = "CHANGE_VALUE_OF"
+    EXECUTE_SCRIPT = "EXECUTE_SCRIPT"
+
+
+@dataclass
+class Event:
+    event_type: EventType
+    current_url: str
+    data: Any
+
+    def __repr__(self):
+        # remove https:// and domain as visual noise
+        url = urlunsplit(urlsplit(self.current_url)._replace(scheme="", netloc=""))
+        return f"Event({self.event_type}, {url=}, {self.data=})"
+
+
+class LoggingEventListener(AbstractEventListener):
+    """
+    Captures all `before_` selenium events and stores them in an array so that they can be printed out for debugging
+    """
+
+    _events: list[Event] = []
+
+    def clear_events(self):
+        self._events = []
+
+    def print_events(self, num_to_print: int = 20):
+        print(f"===== Last {num_to_print} events before test finish =====")
+        for event in self._events[-num_to_print:]:
+            print(event)
+        print("===========================================" + ("=" * len(str(num_to_print))))
+
+    def _add_event(self, event_type: EventType, current_url: str, data: Any):
+        self._events.append(Event(event_type=event_type, current_url=current_url, data=data))
+
+    # --------- event hooks --------- #
+
+    def before_navigate_to(self, url: str, driver) -> None:
+        self._add_event(EventType.NAVIGATE_TO, driver.current_url, url)
+
+    def before_find(self, by, value, driver) -> None:
+        self._add_event(EventType.FIND, driver.current_url, (by, value))
+
+    def before_click(self, element, driver) -> None:
+        self._add_event(EventType.CLICK, driver.current_url, element)
+
+    def before_change_value_of(self, element, driver) -> None:
+        self._add_event(EventType.CHANGE_VALUE_OF, driver.current_url, element)
+
+    def before_execute_script(self, script, driver) -> None:
+        self._add_event(EventType.EXECUTE_SCRIPT, driver.current_url, script)
