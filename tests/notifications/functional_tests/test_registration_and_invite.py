7c32ada72885672e0968c79dc3e8d1863dfc3aa1
Katie Smith <klssmith@users.noreply.github.com>
2025-12-01 09:48:54 +0000

    wip

--- tests/notifications/functional_tests/test_registration_and_invite.py
+++ tests/notifications/functional_tests/test_registration_and_invite.py
@@ -5,9 +5,14 @@ from config import config
 from tests.pages import (
     ChangeName,
     DashboardPage,
+    ServiceJoinRequestApprovePage,
+    ServiceJoinRequestChoosePermissionsPage,
+    ServiceJoinRequestChooseServicePage,
+    ServiceJoinRequestJoinAskPage,
     ServiceSettingsPage,
     SignInPage,
     SmsSenderPage,
+    YourServicesPage,
 )
 from tests.pages.rollups import sign_in
 from tests.test_utils import (
@@ -16,11 +21,12 @@ from tests.test_utils import (
     do_user_can_invite_someone_to_notify,
     do_user_can_update_reply_to_email_to_service,
     do_user_registration,
+    get_link,
     recordtime,
 )
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def user_register_or_sign_in(driver):
     sign_in_page = SignInPage(driver)
     sign_in_page.get()
@@ -50,7 +56,7 @@ def _sign_in_again(driver):
 
 @recordtime
 @pytest.mark.xdist_group(name="registration-flow")
-def test_invite_flow(driver):
+def test_invite_flow(driver, user_register_or_sign_in):
     do_user_can_invite_someone_to_notify(driver, basic_view=False)
 
     _sign_in_again(driver)
@@ -61,14 +67,14 @@ def test_invite_flow(driver):
 @recordtime
 @pytest.mark.xdist_group(name="registration-flow")
 @pytest.mark.skipif("not config['enable_edit_reply_to']")  # condition as string for deferred evaluation
-def test_can_add_and_update_reply_to(driver):
+def test_can_add_and_update_reply_to(driver, user_register_or_sign_in):
     do_user_can_add_reply_to_email_to_service(driver)
     do_user_can_update_reply_to_email_to_service(driver)
 
 
 @recordtime
 @pytest.mark.xdist_group(name="registration-flow")
-def test_can_add_and_update_sms_sender_of_service(driver):
+def test_can_add_and_update_sms_sender_of_service(driver, user_register_or_sign_in):
     dashboard_page = DashboardPage(driver)
     sms_sender_page = SmsSenderPage(driver)
 
@@ -100,7 +106,7 @@ def test_can_add_and_update_sms_sender_of_service(driver):
 
 @recordtime
 @pytest.mark.xdist_group(name="registration-flow")
-def test_change_service_name(driver):
+def test_change_service_name(driver, user_register_or_sign_in):
     old_name = config["service_name"]
     new_name = f"{old_name} (renamed)"
 
@@ -130,3 +136,69 @@ def test_change_service_name(driver):
 
     dashboard_page.go_to_dashboard_for_service()
     assert dashboard_page.get_service_name() == old_name
+
+
+def _do_approver_sign_in(driver):
+    requested_user_email = config["service"]["seeded_user"]["email"]
+    sign_in(driver, account_type="seeded")
+    invite_link = get_link(config["notify_templates"]["request_invite_to_service_template_id"], requested_user_email)
+    driver.get(invite_link)
+
+
+def _do_request_to_join_service(driver):
+    add_service_page = YourServicesPage(driver)
+    add_service_page.wait_until_current()
+    add_service_page.join_existing_service()
+
+    add_service_page.wait_until_url_contains("/join-a-service/choose")
+
+    choose_service_page = ServiceJoinRequestChooseServicePage(driver)
+    if choose_service_page.check_if_search_input_exists():
+        choose_service_page.search_service_input = "Functional tests"
+    choose_service_page.go_to_selected_service()
+
+    join_ask_page = ServiceJoinRequestJoinAskPage(driver)
+    join_ask_page.select_approver_user_checkbox()
+    join_ask_page.request_reason_input = "Test join service reason"
+    join_ask_page.ask_to_join_service()
+
+    join_ask_page.wait_until_url_contains("/join/you-have-asked?number_of_users_emailed=1")
+
+    add_service_page.sign_out()
+
+
+def _do_approver_approved_request(driver):
+    _do_approver_sign_in(driver)
+
+    join_service_request_approve_page = ServiceJoinRequestApprovePage(driver)
+    join_service_request_approve_page.continue_to_next_step()
+
+    choose_permissions_page = ServiceJoinRequestChoosePermissionsPage(driver)
+    choose_permissions_page.fill_invitation_form()
+    choose_permissions_page.select_sms_auth_form()
+    choose_permissions_page.save_permissions()
+    choose_permissions_page.wait_until_url_contains("/users")
+
+
+def _do_approver_rejected_request(driver):
+    _do_approver_sign_in(driver)
+
+    join_service_request_approve_page = ServiceJoinRequestApprovePage(driver)
+    join_service_request_approve_page.select_rejected_option()
+    join_service_request_approve_page.continue_to_next_step()
+
+    join_service_request_approve_page.wait_until_url_contains("/refused")
+
+
+@recordtime
+@pytest.mark.xdist_group(name="registration-flow")
+def test_join_existing_service_rejected_flow(driver, user_register_or_sign_in):
+    _do_request_to_join_service(driver)
+    _do_approver_rejected_request(driver)
+
+
+@recordtime
+@pytest.mark.xdist_group(name="registration-flow")
+def test_join_existing_service_approved_flow(driver, user_register_or_sign_in):
+    _do_request_to_join_service(driver)
+    _do_approver_approved_request(driver)
