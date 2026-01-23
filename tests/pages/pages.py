64f1425f934833edf1d0a612c3001f59fe89296f
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2026-01-23 09:45:24 +0000

    TMP

--- tests/pages/pages.py
+++ tests/pages/pages.py
@@ -1053,6 +1053,7 @@ class CheckEmergencyContactListPage(BasePage):
 
     def get_preview_header(self):
         cells = self.wait_for_elements(self.preview_header_cells)
+        print(cells)
         all_contents = [cell.text for cell in cells]
         assert all_contents[0] == "1"
         return all_contents[1:]
